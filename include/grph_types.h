23e43966610a6c87c414de2c53396b375339301a
Emil Pedersen <emil.pedersen@epitech.eu>
2022-01-27 19:41:46 +0100

    Add grph_string_t and printOut/Err

--- include/grph_types.h
+++ include/grph_types.h
@@ -1,14 +1,18 @@
 //
 //  grph_types.h
-//  GRPH Standard Library
+//  GRPH stdlib - libgrph
 //
 //  Created by Emil Pedersen on 26/01/2022.
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
 
-#ifndef grph_types_h
-#define grph_types_h
+#ifndef GRPH_TYPES_H
+#define GRPH_TYPES_H
 
-#include <inttypes.h>
+#include <stdint.h>
 
 typedef int64_t grph_int_t;
 typedef double grph_float_t;
@@ -20,4 +24,11 @@ struct {
     grph_float_t y;
 } typedef grph_pos_t;
 
-#endif /* grph_types_h */
+struct {
+    uint64_t metadata;
+    char *buffer;
+} typedef grph_string_t;
+
+#define STR_LEN(str) ((str).metadata & ~(1ULL << 63))
+
+#endif /* GRPH_TYPES_H */
---
+++ sources/libgrph/stdio.c
@@ -0,0 +1,24 @@
+//
+//  stdio.c
+//  GRPH stdlib - libgrph
+//
+//  Created by Emil Pedersen on 27/01/2022.
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+#include "grph_types.h"
+
+#include <unistd.h>
+
+void grph_stdio_printOut(grph_string_t message)
+{
+    write(1, message.buffer, STR_LEN(message));
+}
+
+void grph_stdio_printErr(grph_string_t message)
+{
+    write(2, message.buffer, STR_LEN(message));
+}
