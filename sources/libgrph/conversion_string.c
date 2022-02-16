4331456c573e26611ad10f7ab464f2fb82239820
Snowy <3mille.prenom.nom@gmail.com>
2022-02-16 18:25:40 +0100

    Rewrite `grphas_string` in GRPH directly

--- sources/libgrph/conversion_string.c
+++ sources/libgrph/conversion_string.c
@@ -16,35 +16,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static grph_string_t int_to_string(grph_integer_t number)
+grph_string_t int_to_string(grph_integer_t number)
 {
     char *box = malloc(21); // max size is 20 + \0
     grph_integer_t len = sprintf(box, "%lld", number);
     return (grph_string_t) { (0b010ULL << 61) | len, box };
 }
 
-static grph_string_t float_to_string(grph_float_t number)
+grph_string_t float_to_string(grph_float_t number)
 {
     char *box = malloc(32); // idk, swift uses 32 bytes
     grph_integer_t len = snprintf(box, 32, "%f", number);
     return (grph_string_t) { (0b010ULL << 61) | len, box };
-}
-
-// because of stupid ABI stuff, if expecting a struct, it will be passed by pointer instead of being unpacked
-grph_string_t grphas_string(typeid val_type, void *val_data0, void *val_data1, void *val_data2)
-{
-    struct grph_existential val = { val_type, { val_data0, val_data1, val_data2} };
-    switch ((existential_type_id_t) *val.type) {
-    case EXISTENTIAL_ID_void:
-        return grph_string_literal("void.VOID");
-    case EXISTENTIAL_ID_boolean:
-        return val.data[0] ? grph_string_literal("true") : grph_string_literal("false");
-    case EXISTENTIAL_ID_integer:
-        return int_to_string(*(grph_integer_t *) &val.data[0]);
-    case EXISTENTIAL_ID_float:
-        return float_to_string(*(grph_float_t *) &val.data[0]);
-    case EXISTENTIAL_ID_string:
-        return *(grph_string_t *) &val.data[0];
-    }
-    return grph_string_literal("<?invalid?>");
 }
