8fa60b15ea614876694e3f9a54621efcd9820024
Snowy <3mille.prenom.nom@gmail.com>
2023-06-10 03:05:42 +0200

    Implement runtime type getter in stdlib

--- sources/libgrph/fields.c
+++ sources/libgrph/fields.c
@@ -12,8 +12,29 @@
 #include "grph_fields.h"
 #include "grph_types.h"
 #include "grph_string_t.h"
+#include "typetable.h"
+#include "existentials.h"
+
+#include <stdio.h>
 
 grph_integer_t grphp_string_length_get(grph_string_t this)
 {
     return grph_string_get_length(this);
+}
+
+static bool type_is_ref(struct typetable *tt)
+{
+    return ((TYPETABLE_TYPEID_CHAR(tt) >= 64 && TYPETABLE_TYPEID_CHAR(tt) < 128)
+            || TYPETABLE_TYPEID_CHAR(tt) == EXISTENTIAL_ID_array
+            || TYPETABLE_TYPEID_CHAR(tt) == EXISTENTIAL_ID_multi_or_ref);
+}
+
+struct typetable *grphp_mixed_type_get(struct grph_existential *this)
+{
+    struct typetable *easy = this->type;
+
+    if (type_is_ref(easy)) {
+        return *(struct typetable **) this->data[0];
+    }
+    return easy;
 }
