9d939ead5369767824e634c42c99f4acbb56cb47
Snowy <3mille.prenom.nom@gmail.com>
2023-06-11 19:43:45 +0200

    Add pointer-returning function for shape fields

--- sources/libgrph/shapes/shape_fields.c
+++ sources/libgrph/shapes/shape_fields.c
@@ -14,17 +14,27 @@
 
 #include <stddef.h>
 
+#define GET_FROM_PTR(name, type) \
+type grphp_shape_##name##_get(grph_shape_t *shape) \
+{ \
+    return *grphp_shape_##name##_ptr(&shape); \
+}
+
 #define SWT_FIELD(name, type) \
-type grphp_shape_##name##_get(grph_shape_t *shape) \
+type *grphp_shape_##name##_ptr(grph_shape_t **shape) \
 { \
-    size_t offset = shape->isa->vwt->swt->name##_offset; \
+    size_t offset = (*shape)->isa->vwt->swt->name##_offset; \
     void *opaque = shape; \
-    return *(type *) (opaque + offset); \
-}
+    return opaque + offset; \
+} \
+GET_FROM_PTR(name, type)
+
 #define STROKE_WRAPPER_FIELD(name, type) \
-type grphp_shape_##name##_get(grph_shape_t *shape) \
+type *grphp_shape_##name##_ptr(grph_shape_t **shape) \
 { \
-    return grphp_shape_strokeWrapper_get(shape).name; \
-}
+    return &grphp_shape_strokeWrapper_ptr(shape)->name; \
+} \
+GET_FROM_PTR(name, type)
+
 #include "shape_witness.inc"
 #undef SWT_FIELD
